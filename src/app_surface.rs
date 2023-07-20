200895430893b8ed559cdb994f2ca84a12ad84fb
Ad <DeadLimit@lkr5.onmicrosoft.com>
2023-07-20 08:01:58 +0800

    update

---
+++ src/app_surface.rs
@@ -0,0 +1,22 @@
+pub struct AppSurface {
+    pub view: winit::window::Window,
+    pub scale_factor: f32,
+    pub maximum_frames: i32,
+    pub sdq: crate::SurfaceDeviceQueue,
+    pub callback_to_app: Option<extern "C" fn(arg: i32)>,
+    pub temporary_directory: &'static str,
+    pub library_directory: &'static str,
+}
+
+impl AppSurface {
+    pub async fn new(view: winit::window::Window) -> Self {
+        let scale_factor = view.scale_factor();
+        let default_backends = if cfg!(feature = "webgl") {
+            wgpu::Backends::all()
+        } else {
+            wgpu::Backends::PRIMARY
+        };
+        let backend = wgpu::util::backend_bits_from_env().unwrap_or(default_backends);
+    }
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use std::{ops::Deref, sync::Arc};
+
+use app_surface::AppSurface;
+use winit::event::Touch;
+
+mod app_surface;
 mod framework;
 
 pub fn log_print(text: &str) {
@@ -6,4 +12,36 @@ pub fn log_print(text: &str) {
 
     #[cfg(not(target_arch = "wasm32"))]
     println!("{text}");
+}
+
+pub struct SurfaceDeviceQueue {
+    pub surface: wgpu::Surface,
+    pub config: wgpu::SurfaceConfiguration,
+    pub adapter: wgpu::Adapter,
+    pub device: Arc<wgpu::Device>,
+    pub queue: Arc<wgpu::Queue>,
+}
+
+impl SurfaceDeviceQueue {
+    pub fn update_config_format(&mut self, format: wgpu::TextureFormat) {
+        self.config.format = format;
+        self.config.view_formats = vec![format.add_srgb_suffix(), format.remove_srgb_suffix()];
+        self.surface.configure(&self.device, & self.config);
+    }
+}
+
+impl Deref for AppSurface {
+    type Target = SurfaceDeviceQueue;
+    fn deref(&self) -> &Self::Target {
+        &self.sdq
+    }
+}
+
+pub trait SurfaceFrame {
+    fn view_size(&self) -> ViewSize;
+    fn resize_surface(&mut self);
+    fn pintch(&mut self, _touch: Touch) {}
+    fn touch(&mut self, _touch: Touch) {}
+    // TODO lots of methods
+    // see github.com/jinleili/wgpu-in-app/blob/master/app-surface/src/lib.rs
 }
