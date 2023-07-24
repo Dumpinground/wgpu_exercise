112d008f4b78a6c25dd998ed65e19c1da89f5e15
Dumpinground <e60533658@icloud.com>
2023-07-25 00:10:03 +0800

    fixed

--- examples/tutorial_3_pipeline/main.rs
+++ examples/tutorial_3_pipeline/main.rs
@@ -1,14 +1,17 @@
 use wgpu_exercise::log_print;
-use winit::event_loop::EventLoop;
+use winit::{event_loop::EventLoop, window::Window};
 
 mod challenge;
 mod standard;
 
 fn main() {
+    let event_loop = EventLoop::new();
+    let window = winit::window::Window::new(&event_loop).unwrap();
+
     #[cfg(not(target_arch = "wasm32"))]
     {
         env_logger::init();
-        pollster::block_on(app());
+        pollster::block_on(app(event_loop, window));
     }
 
     #[cfg(target_arch = "wasm32")]
@@ -24,14 +27,11 @@ fn main() {
                     .ok()
             })
             .expect("couldn't append canvas to document body");
-        wasm_bindgen_futures::spawn_local(app());
+        wasm_bindgen_futures::spawn_local(app(event_loop, window));
     }
 }
 
-async fn app() {
-    let event_loop = EventLoop::new();
-    let window = winit::window::Window::new(&event_loop).unwrap();
-
+async fn app(event_loop: EventLoop<()>, window: Window) {
     if cfg!(feature = "challenge") {
         log_print("run challenge example");
         challenge::run(event_loop, window).await
